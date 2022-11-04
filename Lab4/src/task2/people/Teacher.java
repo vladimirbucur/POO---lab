9b2ddf931ad9c8b5fd958f73276eb6250b17946a
vladimirbucur <82315968+vladimirbucur@users.noreply.github.com>
2022-11-04 14:01:31 +0200

    Lab4

---
+++ Lab4/src/task2/people/Teacher.java
@@ -0,0 +1,77 @@
+package task2.people;
+
+import task2.database.Database;
+
+import java.util.List;
+
+public class Teacher {
+    private String firstName;
+    private String lastName;
+    private List<String> subjects;
+
+    public Teacher(String firstName, String lastName, List<String> subjects) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.subjects = subjects;
+    }
+
+    public Teacher(Teacher teacher) {
+        this.firstName = teacher.firstName;
+        this.lastName = teacher.lastName;
+        this.subjects = teacher.getSubjects();
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher: " + firstName + " " + lastName + "\n"
+                + "Subjects: " + subjects + "\n";
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<String> getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(List<String> subjects) {
+        this.subjects = subjects;
+    }
+
+    public List<Teacher> getAllTeachers() {
+        return Database.getDatabase().findAllTeachers();
+    }
+
+    public List<Teacher> getTeachersBySubject(String subject) {
+        return Database.getDatabase().findTeachersBySubject(subject);
+    }
+
+    public List<Student> getAllStudents() {
+        return Database.getDatabase().findAllStudents();
+    }
+
+    public List<Student> getStudentsBySubject(String subject) {
+        return Database.getDatabase().getStudentsBySubject(subject);
+    }
+
+    public List<Student> getStudentsByAverageGrade() {
+        return Database.getDatabase().getStudentsByAverageGrade();
+    }
+
+    public List<Student> getStudentsByGradeForSubject(String subject) {
+        return Database.getDatabase().getStudentsBySubject(subject);
+    }
+}
